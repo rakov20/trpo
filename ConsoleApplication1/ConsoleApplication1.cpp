99bde1b4a5c62c882ba96c14b629c8b835502bd4
rakov20 <90721799+rakov20@users.noreply.github.com>
2021-09-21 21:32:37 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -28,17 +28,41 @@ int main()
 	cin >> month;
 	switch (month)
 	{
-	case 1,2,12:
+	case 1:
 		cout << "Зима" << endl;
 		break;
-	case 3,5:
+	case 2:
+		cout << "Зима" << endl;
+		break;
+	case 3:
 		cout << "Весна|" << endl;
 		break;
-	case 6,7,8:
+	case 4:
+		cout << "Весна|" << endl;
+		break;
+	case 5:
+		cout << "Весна|" << endl;
+		break;
+	case 6:
 		cout << "Лето" << endl;
 		break;
-	case 9,10,11:
+	case 7:
+		cout << "Лето" << endl;
+		break;
+	case 8:
+		cout << "Лето" << endl;
+		break;
+	case 9:
 		cout << "Осень" << endl;
+		break;
+	case 10:
+		cout << "Осень" << endl;
+		break;
+	case 11:
+		cout << "Осень" << endl;
+		break;
+	case 12:
+		cout << "Зима" << endl;
 		break;
 	default:
 		cout << month << " месяца не существует" << endl;
